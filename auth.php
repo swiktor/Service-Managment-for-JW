1f3e006a2657c10b72547a54e6132244548d8224
swiktor <swiktor.informatyk@gmail.com>
2020-01-26 22:02:24 +0100

    Add IP checker

--- auth.php
+++ auth.php
@@ -4,3 +4,11 @@
   $_SESSION['id_uzytkownika'] = $_COOKIE['SluzbyID'];
 
 $id_uzytkownika = $_SESSION['id_uzytkownika'];
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
